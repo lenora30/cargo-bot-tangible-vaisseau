a32f91b4bc594d99aeb887a4cabec734fa475dcd
ledac.enora <ledac.enora@etu.unistra.fr>
2022-07-04 15:14:55 +0200

    police

--- cargo-not/py/index.py
+++ cargo-not/py/index.py
@@ -5,11 +5,14 @@
 import markup
 import sys
 
+style = [filename for filename in sys.argv if filename.endswith('.css')]
+style.append('https://fonts.googleapis.com/css2?family=Work+Sans:wght@400&display=swap')
+
 index = markup.page()
 index.init(
     title = 'Cargo-Not: Educational Cargo-Bot',
     script = [filename for filename in sys.argv if filename.endswith('.js')],
-    css = [filename for filename in sys.argv if filename.endswith('.css')])
+    css = style)
 index.script('main();')
 
 print index
