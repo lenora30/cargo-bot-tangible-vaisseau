86a0e47cbfc7acb7dcf2b1d0c8a9a894cd09c2ee
lenora30 <106536306+lenora30@users.noreply.github.com>
2022-07-06 11:51:14 +0000

    Deploying to gh-pages from @ lenora30/cargo-bot-tangible-vaisseau@e82761df356441401829610b18d99ad4cfb096f9 🚀

--- scripts/scanReader.js
+++ scripts/scanReader.js
@@ -70,7 +70,7 @@ function tri_fonction(topcodes, case_height, top_right, top_left, bottom_left, b
     if (!(isVertices(e)) && inFunction(top_left, top_right, bottom_right, bottom_left, e)) {
       if (e.code >= CODE_MIN_COMMAND && e.code <= CODE_MAX_COMMAND || e.code >= CODE_MIN_DIFFICULTY) {   // check if the instruction is a command or a level code
         num_fonc = Math.floor(e.y / case_height);
-
+        console.log(num_fonc);
         if (num_fonc == 2 || num_fonc == 1)
         {
           num_fonc--;
@@ -79,7 +79,7 @@ function tri_fonction(topcodes, case_height, top_right, top_left, bottom_left, b
       } 
     }
   });
-  console.log('tab_points[0]');
+  
   return tab_points;
 }
 
