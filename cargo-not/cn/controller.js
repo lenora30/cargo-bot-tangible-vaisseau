31fbce827eea14c1218e685e81022d4b3ff79122
lenora30 <106536306+lenora30@users.noreply.github.com>
2022-07-07 21:08:49 +0000

    Deploying to gh-pages from @ lenora30/cargo-bot-tangible-vaisseau@c4cfa875e32c1b9c191e3484841eaffa7c1fd236 🚀

--- cargo-not/cn/controller.js
+++ cargo-not/cn/controller.js
@@ -447,8 +447,8 @@ cn.controller.proc_possible = function (game) {
   var level_string;
   var level_info;
   for (i=1; i<10; i++) {
-    console.log(i);
     level_string = cn.constants.LEVEL_CODE[i];
+    console.log(level_string);
     level_info = cn.LevelData.levels[level_string];
     if (game.goal.equals(level_info.goal)) {
       proc = false;
