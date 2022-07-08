560ad579e19fa6ed34f928a61b5cc2d39680e904
ledac.enora <ledac.enora@etu.unistra.fr>
2022-07-08 10:04:58 +0200

    leveldata.goals

--- cargo-not/cn/controller.js
+++ cargo-not/cn/controller.js
@@ -452,7 +452,7 @@ cn.controller.proc_possible = function (game) {
   for (i=1; i<10; i++) {
     level_string = cn.constants.LEVEL_CODE[i];
     level_info = cn.LevelData.levels[level_string];
-    console.log(game.levelData);
+    console.log(game.levelData.goal);
     if (game.level.equals2(level_info)) {
       proc = false;
     }
